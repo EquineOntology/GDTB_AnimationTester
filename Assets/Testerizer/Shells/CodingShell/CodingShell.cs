91d21cad9ea7bfaca4c69fc4536fb0525a79cbf6
HHErebus <christian.fratta@gmail.com>
2015-09-16 23:30:59 +0200

    First real codingShell commit

---
+++ Assets/Testerizer/Shells/CodingShell/CodingShell.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CodingShell : EditorWindow
+{
+    public static string QQQ = "QQQ";
+
+    private List<string> _qqqScripts = new List<string>();
+    private List<string> _qqqs = new List<string>();
+
+    // CONSTANTS (labels, etc).
+    private const string LIST_QQQS = "List QQQs";
+
+    [MenuItem("Testerizer/Load Coding Shell")]
+    public static void Init()
+    {
+        // Get existing open window or if none, make a new one.
+        CodingShell window = (CodingShell)EditorWindow.GetWindow(typeof(CodingShell));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.Space();
+        if (GUILayout.Button(LIST_QQQS))
+        {
+            CodingShellHelper.FindAllScripts();
+            CodingShellHelper.CheckAllScriptsForQQQs(out _qqqScripts, out _qqqs);
+            for (int i = 0; i < _qqqs.Count; i++)
+            {
+                Debug.Log(i + ": " + _qqqs[i] + " (in \"" + _qqqScripts[i] + "\").");
+            }
+        }
+        EditorGUILayout.Space();
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Testerizer/Shells/CodingShell/CodingShellHelper.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public static class CodingShellHelper
+{
+    public static List<string> FindAllScripts()
+    {        
+        var allAssets = AssetDatabase.GetAllAssetPaths();
+        var allScripts = new List<string>();
+        
+        foreach (var path in allAssets)
+        {
+            if (path.EndsWith(".cs") || path.EndsWith(".js"))
+            {
+                allScripts.Add(path);
+            }
+        }///QQQ bananas!
+        return allScripts;
+    }
+    
+    public static void CheckAllScriptsForQQQs(out List<string> scripts, out List<string> qqqs)
+    {
+        // Since we're rechecking everything, let's clear the two lists.
+        scripts = new List<string>();
+        qqqs = new List<string>();
+        
+        // First we collect all scripts in the project, and then we check them for QQQs.
+        var AllScripts = FindAllScripts();
+        foreach(var script in AllScripts)
+        {
+            var QQQsInScript = CheckScriptForQQQs(script);
+            
+            for(int i = 0; i < QQQsInScript.Count; i++)
+            {
+                // Since the string "QQQ" is repeated many times in these two files, its default value would give
+                // a bunch of false positives in these two files. So either the token doesn't use the default value,
+                // or we exclude these two files from the collection.
+                if (CodingShell.QQQ != "QQQ" || (!script.EndsWith("CodingShell.cs") && !script.EndsWith("CodingShellHelper.cs")))
+                {
+                    scripts.Add(script);
+                    qqqs.Add(QQQsInScript[i]);
+                }
+            }
+        }
+    }
+    
+//QQQMelonas
+    private static List<string> CheckScriptForQQQs(string path)
+    {
+        var currentQQQs = new List<string>();
+
+        var lines = File.ReadAllLines(path);
+        string completeQQQ;            
+        for (int i = 0; i < lines.Length; i++)
+        {
+            completeQQQ = "";
+            if (lines[i].Contains(CodingShell.QQQ))
+            {
+                var index = lines[i].IndexOf(CodingShell.QQQ);
+                var tempString = lines[i].Substring(index);
+                tempString = tempString.Substring(CodingShell.QQQ.Length);
+                tempString.Trim();
+                completeQQQ += tempString;
+                
+                currentQQQs.Add(completeQQQ);
+            }            
+        }
+        return currentQQQs;
+    }
+}
