ae3b0f725607288a70a1fd8344e63f52a79dd61b
HHErebus <christian.fratta@gmail.com>
2015-09-17 23:02:21 +0200

    Window & list functionality

--- Assets/Testerizer/Shells/CodingShell/CodingShell.cs
+++ Assets/Testerizer/Shells/CodingShell/CodingShell.cs
@@ -1,41 +1,96 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 using System.Collections.Generic;
 
 public class CodingShell : EditorWindow
 {
-    public static string QQQ = "QQQ";
+    public static string QQQTemplate = "QQQ";
+    public static int charLimitBeforeNewline = 60;
 
+    // QQQ
+    public ReorderableList _reorderableQQQs;
+    public List<QQQ> _qqqs = new List<QQQ>();
+
+    //private List<int> _priorities = new List<int>();
     private List<string> _qqqScripts = new List<string>();
-    private List<string> _qqqs = new List<string>();
+    private List<string> _qqqTasks = new List<string>();
 
     // CONSTANTS (labels, etc).
-    private const string LIST_QQQS = "List QQQs";
+    private const int BUTTON_WIDTH = 100;
+    private const int BOX_WIDTH = 400;
+    private const string LIST_QQQS = "Refresh list";
 
     [MenuItem("Testerizer/Load Coding Shell")]
     public static void Init()
-    {
+    {       
         // Get existing open window or if none, make a new one.
         CodingShell window = (CodingShell)EditorWindow.GetWindow(typeof(CodingShell));
         window.Show();
     }
 
+    public void OnEnable()
+    {
+        _qqqs = CodingShellHelper.CheckAllScriptsForQQQs(out _qqqTasks, out _qqqScripts);
+        /*_reorderableQQQs = new ReorderableList(_qqqs, typeof(QQQ), true, true, true, true);
+
+        _reorderableQQQs.drawElementBackgroundCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            var element = _reorderableQQQs.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+            EditorGUI.PropertyField(
+                new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("Task"));
+            EditorGUI.PropertyField(
+                new Rect(rect.x + 60, rect.y, rect.width - 60 - 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("Script"));
+        };*/
+    }
+
     private void OnGUI()
     {
-        EditorGUILayout.BeginHorizontal();
-
+        EditorGUILayout.BeginVertical();
+        DrawQQQList();
         EditorGUILayout.Space();
-        if (GUILayout.Button(LIST_QQQS))
+        DrawListButton();
+        EditorGUILayout.BeginVertical();
+    }
+    
+    private void DrawQQQList()
+    {
+        EditorGUILayout.BeginVertical();
+        
+        for(int i= 0; i< _qqqs.Count; i++)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox, GUILayout.Width(BOX_WIDTH));
+            
+            EditorGUILayout.BeginHorizontal();            
+            GUILayout.Space(10);
+            EditorGUILayout.LabelField(_qqqs[i].Task, EditorStyles.boldLabel);            
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.LabelField("In \"" + _qqqs[i].Script + "\".");
+
+            EditorGUILayout.EndVertical();
+        }
+        
+        EditorGUILayout.EndVertical();
+
+        //_reorderableQQQs.DoLayoutList();
+        //_reorderableQQQs.DoList(new Rect(20, 20, 200, 200));
+    }
+
+    private void DrawListButton()
+    {
+        EditorGUILayout.BeginHorizontal();    
+        EditorGUILayout.Space();
+        if (GUILayout.Button(LIST_QQQS, GUILayout.Width(BUTTON_WIDTH)))
         {
             CodingShellHelper.FindAllScripts();
-            CodingShellHelper.CheckAllScriptsForQQQs(out _qqqScripts, out _qqqs);
-            for (int i = 0; i < _qqqs.Count; i++)
-            {
-                Debug.Log(i + ": " + _qqqs[i] + " (in \"" + _qqqScripts[i] + "\").");
-            }
+            CodingShellHelper.CheckAllScriptsForQQQs(out _qqqTasks, out _qqqScripts);
         }
         EditorGUILayout.Space();
-
         EditorGUILayout.EndHorizontal();
     }
+
+    
 }
--- Assets/Testerizer/Shells/CodingShell/CodingShellHelper.cs
+++ Assets/Testerizer/Shells/CodingShell/CodingShellHelper.cs
@@ -9,22 +9,22 @@ public static class CodingShellHelper
     {        
         var allAssets = AssetDatabase.GetAllAssetPaths();
         var allScripts = new List<string>();
-        
+
         foreach (var path in allAssets)
         {
             if (path.EndsWith(".cs") || path.EndsWith(".js"))
             {
                 allScripts.Add(path);
             }
-        }///QQQ bananas!
+        }
         return allScripts;
     }
     
-    public static void CheckAllScriptsForQQQs(out List<string> scripts, out List<string> qqqs)
+    public static List<QQQ> CheckAllScriptsForQQQs(out List<string> qqqTasks, out List<string> scripts)
     {
         // Since we're rechecking everything, let's clear the two lists.
         scripts = new List<string>();
-        qqqs = new List<string>();
+        qqqTasks = new List<string>();
         
         // First we collect all scripts in the project, and then we check them for QQQs.
         var AllScripts = FindAllScripts();
@@ -34,19 +34,26 @@ public static class CodingShellHelper
             
             for(int i = 0; i < QQQsInScript.Count; i++)
             {
-                // Since the string "QQQ" is repeated many times in these two files, its default value would give
-                // a bunch of false positives in these two files. So either the token doesn't use the default value,
-                // or we exclude these two files from the collection.
-                if (CodingShell.QQQ != "QQQ" || (!script.EndsWith("CodingShell.cs") && !script.EndsWith("CodingShellHelper.cs")))
+                // Since the string "QQQ" is repeated many times in these three files listed, its default value would give
+                // a bunch of false positives in these files. So either the token doesn't use the default value,
+                // or we exclude these three files from the collection.
+                if (CodingShell.QQQTemplate != "QQQ" || (!script.EndsWith("CodingShell.cs") && !script.EndsWith("CodingShellHelper.cs") && !script.EndsWith("QQQ.cs")))
                 {
                     scripts.Add(script);
-                    qqqs.Add(QQQsInScript[i]);
+                    qqqTasks.Add(QQQsInScript[i]);
                 }
             }
         }
+
+        var qqqs = new List<QQQ>();
+        for(int j = 0; j <scripts.Count; j++)
+        {
+            qqqs.Add(new QQQ(qqqTasks[j], scripts[j]));
+        }
+
+        return qqqs;
     }
     
-//QQQMelonas
     private static List<string> CheckScriptForQQQs(string path)
     {
         var currentQQQs = new List<string>();
@@ -56,11 +63,11 @@ public static class CodingShellHelper
         for (int i = 0; i < lines.Length; i++)
         {
             completeQQQ = "";
-            if (lines[i].Contains(CodingShell.QQQ))
+            if (lines[i].Contains(CodingShell.QQQTemplate))
             {
-                var index = lines[i].IndexOf(CodingShell.QQQ);
+                var index = lines[i].IndexOf(CodingShell.QQQTemplate);
                 var tempString = lines[i].Substring(index);
-                tempString = tempString.Substring(CodingShell.QQQ.Length);
+                tempString = tempString.Substring(CodingShell.QQQTemplate.Length);
                 tempString.Trim();
                 completeQQQ += tempString;
                 
