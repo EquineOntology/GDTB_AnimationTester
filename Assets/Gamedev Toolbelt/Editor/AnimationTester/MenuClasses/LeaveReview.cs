e96b1ba4e660b53592a0a5e6c232291a6299dbfa
Christian Fratta <christian.fratta@gmail.com>
2016-10-31 00:03:33 +0100

    Add links for review and other IH assets to AnimationTester menu

---
+++ Assets/Gamedev Toolbelt/Editor/AnimationTester/MenuClasses/LeaveReview.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace com.immortalhydra.gdtb.animationtester
+{
+	public class LeaveReview : MonoBehaviour
+	{
+
+#region METHODS
+
+	[MenuItem("Window/Gamedev Toolbelt/AnimationTester/★ Leave a review ★")]
+	private static void GoToAssetStorePage()
+	{
+		Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/search/page=1/sortby=popularity/query=publisher:15617&src=animationtester_menu");
+	}
+
+#endregion
+
+	}
+}
---
+++ Assets/Gamedev Toolbelt/Editor/AnimationTester/MenuClasses/OtherAssetsFromIH.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace com.immortalhydra.gdtb.animationtester
+{
+	public class OtherAssetsFromIH : MonoBehaviour
+	{
+
+#region METHODS
+
+	[MenuItem("Window/Gamedev Toolbelt/AnimationTester/Our other Assets")]
+	private static void GoToAssetStorePage()
+	{
+		Application.OpenURL("https://www.assetstore.unity3d.com/#!/content/70010?src=animationtester_menu");
+	}
+
+#endregion
+
+	}
+}
