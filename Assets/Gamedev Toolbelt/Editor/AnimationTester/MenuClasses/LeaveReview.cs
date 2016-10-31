ac9bdd1078dcd29a19b60df1c3ca40d82442feb8
Christian Fratta <christian.fratta@gmail.com>
2016-10-31 12:45:21 +0100

    Change stars to hearts in leave a review menu

--- Assets/Gamedev Toolbelt/Editor/AnimationTester/MenuClasses/LeaveReview.cs
+++ Assets/Gamedev Toolbelt/Editor/AnimationTester/MenuClasses/LeaveReview.cs
@@ -8,7 +8,7 @@ namespace com.immortalhydra.gdtb.animationtester
 
 #region METHODS
 
-	[MenuItem("Window/Gamedev Toolbelt/AnimationTester/★ Leave a review ★")]
+	[MenuItem("Window/Gamedev Toolbelt/AnimationTester/❤ Leave a review ❤")]
 	private static void GoToAssetStorePage()
 	{
 		Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/search/page=1/sortby=popularity/query=publisher:15617&src=animationtester_menu");
