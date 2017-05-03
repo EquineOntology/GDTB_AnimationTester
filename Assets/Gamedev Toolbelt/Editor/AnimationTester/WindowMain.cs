0b0013c8debbdf36bc9914c9abee2a5b12b1de11
Christian Fratta <christian@learnbrite.com>
2017-05-03 06:43:44 +0200

    Fix misnamed variable

--- Assets/Gamedev Toolbelt/Editor/AnimationTester/WindowMain.cs
+++ Assets/Gamedev Toolbelt/Editor/AnimationTester/WindowMain.cs
@@ -81,7 +81,7 @@ namespace com.immortalhydra.gdtb.animationtester
                 _clipNamesBackup = AnimationClipHandler.BuildClipNamesBackup(_animators, _animations);
 
             #if !UNITY_5_4_OR_NEWER
-                controllersBackup = AnimatorHandler.BuildControllersBackup(_animators);
+                _controllersBackup = AnimatorHandler.BuildControllersBackup(_animators);
             #endif
             }
         }
